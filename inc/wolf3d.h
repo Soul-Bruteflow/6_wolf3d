87cf32e7b492d88e95e8a7d4418646ee6a77183f
Max Vlad <mvlad@e1r5p14.unit.ua>
2017-06-01 15:57:23 +0300

    0.1.3

--- inc/wolf3d.h
+++ inc/wolf3d.h
@@ -1,11 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvlad <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/01 13:21:31 by mvlad             #+#    #+#             */
+/*   Updated: 2017/06/01 13:21:43 by mvlad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef _WOLF3D_H
 # define _WOLF3D_H
 
-//# include "libft.h"
-//# include "get_next_line.h"
+# include "libft.h"
+# include "get_next_line.h"
 # include <math.h>
 # include <SDL2/SDL.h>
+# include <stdio.h>
 
-#include <stdio.h>
+typedef enum	e_bool
+{
+	false, true
+}				t_bool;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,17 @@ int	main(int ac, char **av)
 	SDL_Init(SDL_INIT_VIDEO);
 
 	// Create a SDL window
-	SDL_Window *window = SDL_CreateWindow("Hello, SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_Window *window = SDL_CreateWindow("wolf3d", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 
 	// Create a renderer (accelerated and in sync with the display refresh rate)
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	int running = 1;
+	SDL_Texture* texture = SDL_CreateTexture (renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 800, 600);
+
+	unsigned char *pixels;
+	pixels = malloc(sizeof(pixels) * 800 * 600 * 4);
+
+	t_bool running = true;
 	SDL_Event event;
 	while(running)
 	{
@@ -21,16 +25,33 @@ int	main(int ac, char **av)
 		{
 			if(event.type == SDL_QUIT)
 			{
-				running = 0;
+				running = false;
 			}
 		}
 
 		// Clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
 		// Draw
+//		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+//		SDL_RenderDrawPoint(renderer, 200, 200);
+
+		for( unsigned int i = 0; i < 1000; i++ )
+		{
+			const unsigned int x = rand() % 800;
+			const unsigned int y = rand() % 600;
+
+			const unsigned int offset = ( 800 * 4 * y ) + x * 4;
+			pixels[ offset + 0 ] = rand() % 256;        // b
+			pixels[ offset + 1 ] = rand() % 256;        // g
+			pixels[ offset + 2 ] = rand() % 256;        // r
+			pixels[ offset + 3 ] = SDL_ALPHA_OPAQUE;    // a
+		}
 
 		// Show what was drawn
+		SDL_UpdateTexture(texture, NULL, &pixels[0], 800 * 4);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
 
