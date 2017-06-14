51f1858217b089a592c39f723621d85a7b233808
bruteflow <bruteflow@iMac-bruteflow.local>
2017-06-14 21:39:11 +0300

    0.7

--- src/map_parser.c
+++ src/map_parser.c
@@ -3,6 +3,23 @@
 void	map_parser(t_wolf *w)
 {
 	parser_first_read(w);
+	w->map_height = w->map_y;
+	w->map_width = w->map_x;
+	w->world_map = (Uint8**)ft_malloc_2d_array(sizeof(w->world_map),
+											   w->map_width, w->map_height);
 	parser_second_read(w);
-	w->world_map = (Uint8**)ft_malloc_2d_array(sizeof(w->world_map), w->map_x, w->map_y);
+
+	int y = 0;
+	int x = 0;
+	while (y < 30)
+	{
+		x = 0;
+		while (x < 30)
+		{
+			printf("%d", w->world_map[y][x]);
+			x++;
+		}
+		printf("\n");
+		y++;
+	}
 }
--- src/parser_second_read.c
+++ src/parser_second_read.c
@@ -1,6 +1,43 @@
 #include "wolf3d.h"
 
+/*
+** 0-9, A-Z = 10-35, a-z = 36-63
+*/
+
+Uint8	parse_map_tile(t_wolf *w, int x)
+{
+	Uint8 tmp = 0;
+
+	tmp = (Uint8)w->line[x];
+	if (tmp >= 48 && tmp <= 57)
+		tmp -= '0';
+	else if (tmp >= 65 && tmp <= 90)
+		tmp -= 55;
+	else if (tmp >= 97 && tmp <= 122)
+		tmp -= 61;
+	return (tmp);
+}
+
 void	parser_second_read(t_wolf *w)
 {
+	int i;
 
+	i = 0;
+	open_file(w);
+	w->map_y = 0;
+	while (w->map_y < w->map_height)
+	{
+		i = get_next_line(w->fd, &w->line);
+		w->map_x = 0;
+		while (w->map_x < w->map_width)
+		{
+			w->world_map[w->map_y][w->map_x] = parse_map_tile(w, w->map_x);
+			w->map_x++;
+		}
+		w->map_y++;
+		free(w->line);
+		close_line(w);
+	}
+	gnl_error(w, i);
+	close_file(w);
 }
